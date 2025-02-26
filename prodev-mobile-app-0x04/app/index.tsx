8341b07dfb7e8cb00abdad6e324a3f1079a9e509
Don Bosco <don965877@gmail.com>
2025-02-26 17:15:09 +0300

    resctified structure

--- prodev-mobile-app-0x04/app/index.tsx
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -1,46 +1,26 @@
-import { View, Text, ImageBackground, Image, TouchableOpacity } from 'react-native';
-import { Link } from 'expo-router';
-import { styles } from '../styles/_mainstyle';
-import { BACKGROUNDIMAGE, HEROLOGO } from '@/constants';
+import { Text, View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { ImageBackground, Dimensions, View, Text, TouchableOpacity, Image } from "react-native";
 
-export default function Home() {
-
-    return ( 
-        <View style={styles.container}>
-            <ImageBackground
+export default function Index() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex:1 }}>
+        <ImageBackground
         source={require("@/assets/images/background-image.png")}
-        style={styles.backgroundImageContainer}
-            >
-        <View style={styles.logoContainer}>
-         <Image source={require("@/assets/images/Logo.png")}/>
-        </View>
-        
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>
-            Find your favourite place here
-          </Text>
-          <View style={styles.titleSubTextContainer}>
-            <Text style={styles.titleSubText}>
-              The best prices for  over 2 million properties worldwide
-            </Text>
-          </View>
-        </View>
-
-        <View style={{ marginTop: 'auto', marginBottom: 40 }}>
-          <View style={styles.buttonGroup}>
-            <Link href="/join" asChild>
-              <TouchableOpacity style={styles.buttonPrimary}>
-                <Text style={styles.buttonPrimaryText}>Join</Text>
-              </TouchableOpacity>
-            </Link>
-            <Link href="../sign" asChild>
-              <TouchableOpacity style={styles.buttonSecondary}>
-                <Text style={styles.buttonSecondaryText}>Sign in</Text>
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
-            </ImageBackground>
-        </View>
-    )
+        style={styles.background}
+        >
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+    </ImageBackground>
+    </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
